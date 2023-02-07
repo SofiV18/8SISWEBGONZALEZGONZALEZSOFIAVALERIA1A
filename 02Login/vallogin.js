2400cc6bcbb7abd2c255a554f7d2920da18f3fb6
sofiv18 <sv.gonzalez.g@edu.utc.mx>
2023-02-06 22:59:31 -0600

    inicio de sesion

--- 02Login/vallogin.js
+++ 02Login/vallogin.js
@@ -1,11 +1,11 @@
-function ir(){
+function validar(){
 
     var c=456987;
     var u="Sofia";
     if(document.form.password.value==c && document.form.login.value==u){
     alert ("Bienvenido");}
     else{
-    alert("Los datos ingresados son incorrectos, intenta de nuevo.");
+    alert("Los datos que ingresaste son incorrectos, intenta nuevamente.");
     }
     
     }
