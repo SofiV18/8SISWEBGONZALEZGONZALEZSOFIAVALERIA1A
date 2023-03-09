2d3a5f8b403cd9d9ae2abfcccf11776c4ae29d6b
sofiv18 <sv.gonzalez.g@edu.utc.mx>
2023-03-08 23:57:18 -0600

    API

---
+++ API/app.js
@@ -0,0 +1,47 @@
+
+
+function getCharacter(done){
+
+    const results = fetch('https://rickandmortyapi.com/api/character')
+
+
+    results
+    .then(response => response.json())
+    .then( data => {
+        done(data)
+
+    }) 
+
+}
+
+getCharacter (data => {
+
+    data.results.forEach(personaje =>{
+
+        const article = document.createRange().createContextualFragment(`
+        
+        <article>
+
+        <div class="image-container">
+            <img src="${personaje.image}" alt="personaje">
+
+        </div>
+
+        <h2>${personaje.name}</h2>
+        <span>${personaje.status}</span>
+
+        </article>
+        
+        `)
+
+        const main = document.querySelector('main')
+
+        main.append(article)
+
+
+
+    })
+
+    
+
+})
